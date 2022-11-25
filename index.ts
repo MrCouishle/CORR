77f307fbd270a969eaadf43ca3ec3993d7609162
Santiago Lozada <davidsantiagolozada@gmail.com>
2022-11-25 14:06:04 -0500

    Cambios

--- index.ts
+++ index.ts
@@ -35,7 +35,7 @@ app.use("/api", routes.HOLDING);
 app.use("/api", routes.CARGOPS);
 app.use("/api", routes.CORRES);
 app.use("/api", routes.CORRESINFOR)
-app.use("/api", routes.MACORR);
+// app.use("/api", routes.MACORR);
 // app.use("/api", routes.ACTIV);
 // app.use("/api", routes.CIUDA);
 // app.use("/api", routes.CORRES);
@@ -45,7 +45,6 @@ app.use("/api", routes.MACORR);
 app.use("/api", routes.Entrys);
 app.use("/api", routes.CON802)
 app.use("/api", routes.LISTADORESCORR)
-
 
 //********Routes*******
 
