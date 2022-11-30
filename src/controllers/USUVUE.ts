04914d4768b1a9ec028e0ea1d19ce04468291e86
VictorCobo2 <90394098+VictorCobo2@users.noreply.github.com>
2022-11-30 14:01:00 -0500

    cambios

--- src/controllers/USUVUE.ts
+++ src/controllers/USUVUE.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express";
-import { get_response, get_all_response, omitirId, generarJwt } from "../global/global";
+import {
+  get_response,
+  get_all_response,
+  omitirId,
+  generarJwt,
+  edit_response,
+} from "../global/global";
 import { usuvue_model } from "../models/USUVUE";
 
 export const getusuvue = async (req: Request, res: Response) => {
@@ -10,43 +16,69 @@ export const getusuvue = async (req: Request, res: Response) => {
       {
         $lookup: {
           from: "restr",
-          let: { llave: "$llaveOper"},
+          let: { llave: "$llaveOper" },
           pipeline: [
             {
               $match: {
                 $expr: { $eq: ["$llaveRest.oper", "$$llave"] },
               },
-            },{
-              $project:{
-                _id:0,
-                opc: {$concat:[{$replaceAll: { input: "$llaveRest.opc", find: " ", replacement: ""}}]},
-              }
+            },
+            {
+              $project: {
+                _id: 0,
+                opc: {
+                  $concat: [
+                    {
+                      $replaceAll: {
+                        input: "$llaveRest.opc",
+                        find: " ",
+                        replacement: "",
+                      },
+                    },
+                  ],
+                },
+              },
             },
           ],
           as: "restr",
         },
       },
       {
-        $match:{
-          llaveOper: llaveResp
-        }
+        $match: {
+          llaveOper: llaveResp,
+        },
       },
-     ])
+    ]);
     if (data[0]) {
       if (data[0].clave == atob(clave)) {
-          const token = await generarJwt(data[0].llaveOper)
-        if(atob(clave) === "NUEVO123"){
-            delete data[0].clave
-            res.json({data:data[0], token, changePassword:true})
-        }else res.json({data, token})
+        const token = await generarJwt(data[0].llaveOper);
+        if (atob(clave) === "NUEVO123") {
+          delete data[0].clave;
+          res.json({ data: data[0], token, changePassword: true });
+        } else res.json({ data, token });
       } else res.json({ msg: "USER" });
     } else {
       get_response("usuvue", data, "", res);
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.json({ msg: error });
   }
+};
+
+export const cambiarContra = async (req: Request, res: Response) => {
+  try {
+    const { nueva_pass, pass, llave } = req.params;
+
+    const user = await usuvue_model.findOne({$and:[{llaveOper:llave},{clave:pass}]})
+
+    if(user){
+      const data = await usuvue_model.updateOne({ llaveRest: llave },{$set: {clave: nueva_pass,}});
+      edit_response("usuvue", data, llave, res)
+    }
+
+
+  } catch (error) {}
 };
 
 export const f8Usuvue = async (req: Request, res: Response) => {
@@ -54,14 +86,19 @@ export const f8Usuvue = async (req: Request, res: Response) => {
     const { desde, cantidad } = req.params;
     let { dato } = req.query;
     const data = await usuvue_model
-    .find ({ $or: [
-      { llaveOper: { $regex: dato,  $options: "ix"} },
-      {nombre: { $regex: dato, $options: "i"} }
-    ]}, omitirId)
-    .skip(Number(desde))
-    .limit(Number(cantidad));
+      .find(
+        {
+          $or: [
+            { llaveOper: { $regex: dato, $options: "ix" } },
+            { nombre: { $regex: dato, $options: "i" } },
+          ],
+        },
+        omitirId
+      )
+      .skip(Number(desde))
+      .limit(Number(cantidad));
     get_all_response(data, res);
   } catch (error) {
-    res.json({ msg:error });
+    res.json({ msg: error });
   }
-}
+};
