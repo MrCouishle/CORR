eec1794e633d997c28b81b893d0fbfda44946726
MoCouishle <davidsantiagolozada@gmail.com>
2022-11-30 14:07:09 -0500

    ruta

--- src/routes/USUVUE.ts
+++ src/routes/USUVUE.ts
@@ -4,5 +4,5 @@ import { f8Usuvue, getusuvue, getUsuvueLlave } from '../controllers/USUVUE'
 export const route_usuvue = express.Router()
 
 route_usuvue.get("/usuvue/:llaveResp/:clave", getusuvue);
-route_usuvue.get("/usuvueLlave/:llaveOper", getUsuvueLlave);
+route_usuvue.get("/usuvue&llave/:llaveOper", getUsuvueLlave);
 route_usuvue.get("/f8&usuvue/:desde/:cantidad", f8Usuvue);
