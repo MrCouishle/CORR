eb778f64c6e887f81ea5ac9657526452bb563b58
Cristian David Hernandez Suarez <69637534+cristian-hernandez20@users.noreply.github.com>
2022-12-06 11:24:48 -0500

    cambios

--- src/routes/DNHABIL.ts
+++ src/routes/DNHABIL.ts
@@ -2,9 +2,9 @@ import express from "express";
 
 import { agregarDia, buscarDia, eliminarDia, editarDia } from "../controllers/DNHABIL";
 
-export const router_favoritos = express.Router();
+export const router_dn_habil = express.Router();
 
-router_favoritos.put("/eliminarfavoritos/:usuario", eliminarDia);
-router_favoritos.put("/agregarfavorito/:usuario", agregarDia);
-router_favoritos.get("/favoritos/:usuario/:modulo", buscarDia);
-router_favoritos.get("/favoritos/:usuario/:modulo", editarDia);
+router_dn_habil.put("/eliminarfavoritos/:usuario", eliminarDia);
+router_dn_habil.put("/agregarfavorito/:usuario", agregarDia);
+router_dn_habil.get("/favoritos/:usuario/:modulo", buscarDia);
+router_dn_habil.get("/favoritos/:usuario/:modulo", editarDia);
