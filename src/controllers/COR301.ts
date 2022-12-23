55cc1e3cf492e6f174edae354e0e57eec2ea45b6
Santiago Lozada <100322634+MrCouishle@users.noreply.github.com>
2022-12-23 15:56:47 -0500

    Cambios 301

--- src/controllers/COR301.ts
+++ src/controllers/COR301.ts
@@ -21,7 +21,7 @@ export const getImpresionCorr = async (req: Request, res: Response) => {
       jornada,
       proceden,
       manejo,
-      estado
+      estado,
     } = req.body;
 
     let nitt = {};
@@ -31,38 +31,38 @@ export const getImpresionCorr = async (req: Request, res: Response) => {
     let procedenn = {};
     let manejoo = {};
     let estadoo = {};
-    
-  //   const festivos = [[10],[],[21],[14,15],[30],[20,27],[4,20],[15],[],[17],[7,14],[8]]; //CUANDO SE LLEGUE AL 2023 QUITAR ESTA LINEA
-  //   // const festivos = [[9],[],[20],[6,7],[1,22],[12,19],[3,20],[7,21],[],[16],[6,13],[8,25]]; ESTE ARRAY ES PARA EL 2023
-  //   function calculoEntrega (festivos, diaIni, diasEntrega ){
-  //     let diaPropuesto = new Date (diaIni.getFullYear(), diaIni.getMonth(), diaIni.getDate());
-  //     let i = 1;
 
-  //     while (diasEntrega > 0) {
-  //       let festivo = false;
-  //       diaPropuesto = new Date (diaIni.getFullYear(), diaIni.getMonth(), diaIni.getDate() + 1);
-  //       if (diaPropuesto.getDay() > 0 && diaPropuesto.getDay() < 6){
-  //       let m = diaPropuesto.getMonth();
-  //       let dia = diaPropuesto.getDate();
+    //   const festivos = [[10],[],[21],[14,15],[30],[20,27],[4,20],[15],[],[17],[7,14],[8]]; //CUANDO SE LLEGUE AL 2023 QUITAR ESTA LINEA
+    //   // const festivos = [[9],[],[20],[6,7],[1,22],[12,19],[3,20],[7,21],[],[16],[6,13],[8,25]]; ESTE ARRAY ES PARA EL 2023
+    //   function calculoEntrega (festivos, diaIni, diasEntrega ){
+    //     let diaPropuesto = new Date (diaIni.getFullYear(), diaIni.getMonth(), diaIni.getDate());
+    //     let i = 1;
 
-  //       for (let d in festivos[m]){
-  //         if (dia === festivos[m][d]){
-  //           festivo = true;
-  //           break;
-  //         }
-  //       }
-  //       if(!festivo){
-  //         diasEntrega--;
-  //       }
-  //     }
-  //     i++
-  //   }
-  //   return diaPropuesto;
-  
-  //   const diasFinal = calculoEntrega(diaIni,10,festivos);
-  //   console.log(`Dia inicial: ${diaIni.toString()}`);
-  //   console.log(`Dia final: ${diasFinal.toString()}`);
-  // }
+    //     while (diasEntrega > 0) {
+    //       let festivo = false;
+    //       diaPropuesto = new Date (diaIni.getFullYear(), diaIni.getMonth(), diaIni.getDate() + 1);
+    //       if (diaPropuesto.getDay() > 0 && diaPropuesto.getDay() < 6){
+    //       let m = diaPropuesto.getMonth();
+    //       let dia = diaPropuesto.getDate();
+
+    //       for (let d in festivos[m]){
+    //         if (dia === festivos[m][d]){
+    //           festivo = true;
+    //           break;
+    //         }
+    //       }
+    //       if(!festivo){
+    //         diasEntrega--;
+    //       }
+    //     }
+    //     i++
+    //   }
+    //   return diaPropuesto;
+
+    //   const diasFinal = calculoEntrega(diaIni,10,festivos);
+    //   console.log(`Dia inicial: ${diaIni.toString()}`);
+    //   console.log(`Dia final: ${diasFinal.toString()}`);
+    // }
     if (nit != "99") nitt = { nit: Number(nit) };
     if (dep != "**") depp = { dep: dep };
     if (tipoCorr != "**") tipoCorrr = { tipoCorres: tipoCorr };
@@ -70,12 +70,15 @@ export const getImpresionCorr = async (req: Request, res: Response) => {
       if (jornada == "M")
         jornadaa = {
           hour: { $lt: 12 },
-        }; //$lt para que tome valores de 12 hacia atras, si le pongo la e tomaria el 12
+        };
+      //$lt para que tome valores de 12 hacia atras, si le pongo la e tomaria el 12
       else jornadaa = { hour: { $gte: 12 } };
     }
     if (proceden != "**") procedenn = { proceden: Number(proceden) };
     if (manejo != "**") manejoo = { manejo: Number(manejo) };
-    if (estado != "**" && estado != null ) {estadoo = { estado: Number(estado)} };
+    if (estado != "**" && estado != null) {
+      estadoo = { estado: Number(estado) };
+    }
 
     const data = await corres_model
       .aggregate([
@@ -133,7 +136,7 @@ export const getImpresionCorr = async (req: Request, res: Response) => {
         {
           $lookup: {
             from: "depco",
-            let: { codigo: "$dep"  },
+            let: { codigo: "$dep" },
             pipeline: [
               {
                 $match: {
@@ -149,25 +152,30 @@ export const getImpresionCorr = async (req: Request, res: Response) => {
       .project({
         _id: 0,
         cont: { $concat: [{ $toString: ["$llave.cont"] }, " -E"] },
-        // radicado: lpad("$cont",7,"0"),
         anoLlave: { $concat: [{ $toString: ["$llave.anoLlave"] }] },
-        fecha:1,
-        fechaR: {$substr: ["$fecha",0,10]},
-        hora: { $concat: [{ $toString: {$hour: "$fecha"}}, ":", { $toString: {$minute:"$fecha"}}]},
+        fecha: 1,
+        fechaR: { $substr: ["$fecha", 0, 10] },
+        hora: {
+          $concat: [
+            { $toString: { $hour: "$fecha" } },
+            ":",
+            { $toString: { $minute: "$fecha" } },
+          ],
+        },
         esta: 1,
-        dep:1,
-        estaR:{
-          $switch:{
-            branches:[
-              {case: {$eq: ["$esta", 1]}, then: "EN TRAMITE"},
-              {case: {$eq: ["$esta", 2]}, then: "VENCIDA"},
-              {case: {$eq: ["$esta", 3]}, then: "RESUELTA"},
-              {case: {$eq: ["$esta", 4]}, then: "RESUELTA"},//Se consulto con encargado de correspondencia daniel, el numero 4 es resuelta tambien, igual que el 6 lo toman como resuelta.
-              {case: {$eq: ["$esta", 5]}, then: "PRORROGA"},
-              {case: {$eq: ["$esta", 6]}, then: "ANULADO"}
+        dep: 1,
+        estaR: {
+          $switch: {
+            branches: [
+              { case: { $eq: ["$esta", 1] }, then: "EN TRAMITE" },
+              { case: { $eq: ["$esta", 2] }, then: "VENCIDA" },
+              { case: { $eq: ["$esta", 3] }, then: "RESUELTA" },
+              { case: { $eq: ["$esta", 4] }, then: "RESUELTA" }, //Se consulto con encargado de correspondencia daniel, el numero 4 es resuelta tambien, igual que el 6 lo toman como resuelta.
+              { case: { $eq: ["$esta", 5] }, then: "PRORROGA" },
+              { case: { $eq: ["$esta", 6] }, then: "ANULADO" },
             ],
-            default:"SIN DEFINIR"
-          }
+            default: "SIN DEFINIR",
+          },
         },
         descrip: 1,
         nit: 1,
@@ -189,6 +197,19 @@ export const getImpresionCorr = async (req: Request, res: Response) => {
             unit: "day",
             amount: { $arrayElemAt: ["$tipc.dias", 0] },
           },
+        },
+        fechaVenceR: {
+          $substr: [
+            {
+              $dateAdd: {
+                startDate: "$fecha",
+                unit: "day",
+                amount: { $arrayElemAt: ["$tipc.dias", 0] },
+              },
+            },
+            0,
+            10,
+          ],
         },
         diasVence: {
           $dateDiff: {
@@ -213,30 +234,42 @@ export const getImpresionCorr = async (req: Request, res: Response) => {
         fold: 1,
         nroFact: 1,
         monto: 1,
-        fecheFactR: { $substr:["$fechaFact",0,10]},
-        horaFechaFact: { $concat: [{ $toString: {$hour: "$fechaFact"}}, ":", { $toString: {$minute:"$fechaFact"}}]},
-        fecheEntreR: { $substr:["$fechaEntre",0,10]},
-        horaFechaEntre: { $concat: [{ $toString: {$hour: "$fechaEntre"}}, ":", { $toString: {$minute:"$fechaEntre"}}]},
+        fecheFactR: { $substr: ["$fechaFact", 0, 10] },
+        horaFechaFact: {
+          $concat: [
+            { $toString: { $hour: "$fechaFact" } },
+            ":",
+            { $toString: { $minute: "$fechaFact" } },
+          ],
+        },
+        fecheEntreR: { $substr: ["$fechaEntre", 0, 10] },
+        horaFechaEntre: {
+          $concat: [
+            { $toString: { $hour: "$fechaEntre" } },
+            ":",
+            { $toString: { $minute: "$fechaEntre" } },
+          ],
+        },
         oper: 1,
         manejo: 1,
         manejoR: {
-            $switch:{
-              branches:[
-                {case:{$eq:["$manejo",1]}, then:"INFORMATIVO"},
-                {case:{$eq:["$manejo",2]}, then:"RESOLUTIVO"}
-              ],
-              default:"SIN DEFINIR"
+          $switch: {
+            branches: [
+              { case: { $eq: ["$manejo", 1] }, then: "INFORMATIVO" },
+              { case: { $eq: ["$manejo", 2] }, then: "RESOLUTIVO" },
+            ],
+            default: "SIN DEFINIR",
           },
         },
         proceden: 1,
         procedenR: {
-          $switch:{
-            branches:[
-              {case:{$eq:["$proceden",1]}, then:"EXTERNO"},
-              {case:{$eq:["$proceden",2]}, then:"INTERNO"}
+          $switch: {
+            branches: [
+              { case: { $eq: ["$proceden", 1] }, then: "EXTERNO" },
+              { case: { $eq: ["$proceden", 2] }, then: "INTERNO" },
             ],
-            default:"SIN DEFINIR"
-        },
+            default: "SIN DEFINIR",
+          },
         },
         llaveResp: {
           $let: {
@@ -251,30 +284,32 @@ export const getImpresionCorr = async (req: Request, res: Response) => {
           },
         },
         fechaRespuesta: {
-          $let: {
+          $substr:[
+          {$let: {
             vars: {},
             in: { $add: [{ $arrayElemAt: ["$rescorr.fecha", 0] }] },
-          },
-        },
+          }},0,10
+          ],
+      },
         // horaFechaRes: { $concat: [{ $toString: {$hour: "$rescorr.fecha"}}, ":", { $toString: {$minute:"$rescorr.fecha"}}]},
         // fehcaVenRes:{
 
         // },
         cargo: { $concat: [{ $arrayElemAt: ["$depc.cargo", 0] }] },
         medioIng: 1,
-        medioIngR:{
-          $switch:{
-          branches:[
-            {case: {$eq: ["$medioIng", 1]}, then: "CORREO CERTIFICADO"},
-            {case: {$eq: ["$medioIng", 2]}, then: "E-MAIL"},
-            {case: {$eq: ["$medioIng", 3]}, then: "PERSONAL"},
-            {case: {$eq: ["$medioIng", 4]}, then: "PERSONAL ESCRITA"},//Se consulto con encargado de correspondencia daniel, el numero 4 es resuelta tambien, igual que el 6 lo toman como resuelta.
-            {case: {$eq: ["$medioIng", 5]}, then: "PERSONAL VERBAL"},
-            {case: {$eq: ["$medioIng", 6]}, then: "PÁGINA WEB"}
-          ],
-          default:"SIN DEFINIR"
-        }
-      },
+        medioIngR: {
+          $switch: {
+            branches: [
+              { case: { $eq: ["$medioIng", 1] }, then: "CORREO CERTIFICADO" },
+              { case: { $eq: ["$medioIng", 2] }, then: "E-MAIL" },
+              { case: { $eq: ["$medioIng", 3] }, then: "PERSONAL" },
+              { case: { $eq: ["$medioIng", 4] }, then: "PERSONAL ESCRITA" }, //Se consulto con encargado de correspondencia daniel, el numero 4 es resuelta tambien, igual que el 6 lo toman como resuelta.
+              { case: { $eq: ["$medioIng", 5] }, then: "PERSONAL VERBAL" },
+              { case: { $eq: ["$medioIng", 6] }, then: "PÁGINA WEB" },
+            ],
+            default: "SIN DEFINIR",
+          },
+        },
       })
 
       .match({
@@ -287,7 +322,7 @@ export const getImpresionCorr = async (req: Request, res: Response) => {
           jornadaa,
           procedenn,
           manejoo,
-          estadoo
+          estadoo,
         ],
       });
 
