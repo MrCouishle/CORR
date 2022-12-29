c145e9b9b72826980d5e8d1151a09472f7a5a722
Santiago Lozada <100322634+MrCouishle@users.noreply.github.com>
2022-12-29 13:41:13 -0500

    Cambios de project en GET RESCORR LLAVE

--- src/controllers/COR403.ts
+++ src/controllers/COR403.ts
@@ -207,72 +207,88 @@ export const getRescorrLlave= async (req:Request, res:Response) => {
     };
     console.log(codResp)
     const data = await rescorr_model
-    .aggregate()
-    .project({
-      _id: 0,
-      codResp: 1,
-      swRadi: 1,
-      fecha: 1,
-      fechaR: { $substr: ["$fecha", 0, 10] },
-      horaFecha: {
-        $concat: [
-          { $toString: { $hour: "$fecha" } },
-          ":",
-          { $toString: { $minute: "$fecha" } },
-        ],
-      },
-      firma: 1,
-      llaveMacro: {
-        $concat: [
-          { $toString: ["$codigoMacro"] },
-          { $toString: ["$clMacro"] },
-        ],
-      },
-      asunto: 1,
-      tabla: 1,
-      respon: 1,
-      cargo: 1,
-      llaveRadi: {
-        $concat: [{ $toString: ["$anoRadi"] }, { $toString: ["$contRadi"] }],
-      },
-      fechaRadi: 1,
-      horaRadi: {
-        $concat: [
-          { $toString: { $hour: "$fechaRadi" } },
-          ":",
-          { $toString: { $minute: "$fechaRadi" } },
-        ],
-      },
-      nit: { $concat: [{ $toString: ["$nit"] }] },
-      tipoCorres: 1,
-      descrip: 1,
-      ser: 1,
-      operdiri: 1,
-      dep: 1,
-      esta: { $concat: [{ $toString: ["$esta"] }] },
-      estaR: {
-        $switch: {
-          branches: [
-            { case: { $eq: ["$esta", 1] }, then: "EN TRAMITE" },
-            { case: { $eq: ["$esta", 2] }, then: "VENCIDA" },
-            { case: { $eq: ["$esta", 3] }, then: "RESUELTA" },
-            { case: { $eq: ["$esta", 4] }, then: "RESUELTA" }, //Se consulto con encargado de correspondencia daniel, el numero 4 es resuelta tambien, igual que el 6 lo toman como resuelta.
-            { case: { $eq: ["$esta", 5] }, then: "PRORROGA" },
-            { case: { $eq: ["$esta", 6] }, then: "ANULADO" },
-          ],
-          default: "SIN DEFINIR",
+    .aggregate([
+      {
+        $lookup: {
+          from: "terce",
+          localField: "nit",
+          foreignField: "codigo",
+          as: "ter",
         },
       },
-      codAuxco: 1,
-      codUnifun: 1,
-      proceden: 1,
-      oper: 1,
-      operModi: 1,
-      medio: 1,
-      numeroFact: 1,
-      monto: 1,
-      nroGuia: 1,
-      perRec: 1, //Esto en el codigo cobol es "presente"
+
+      {
+        $lookup: {
+          from: "tipco",
+          let: { tipoCorres: { $toString: "$tipoCorres" } },
+          pipeline: [
+            {
+              $match: {
+                $expr: { $eq: ["$codigo", "$$tipoCorres"] },
+              },
+            },
+          ],
+          as: "tipco",
+        },
+      },
+    ])
+    .project({
+      _id: 0,
+        codResp2: {
+          $concat: [
+            { $toString: ["$codResp.anoLlave"] },
+            { $toString: ["$codResp.cont"] },
+          ],
+        },
+        codResp: 1,
+        anoLlave: { $concat: [{ $toString: ["$codResp.anoLlave"] }] },
+        contLlave: { $concat: [{ $toString: ["$codResp.cont"] }] },
+        swRadi: 1,
+        fecha: 1,
+        hora: { $hour: "$fecha" },
+        minutos: { $minute: "$fecha" },
+        firma: 1,
+        codigoMacro: 1,
+        asunto: 1,
+        tabla: 1,
+        respon: 1,
+        cargo: 1,
+        llaveRadi: { $concat: [{ $toString: ["$anoRadi"] }, "$contRadi"] },
+        fechaRadi: 1,
+        horaRadi: { $hour: "$fechaRadi" },
+        nit: { $concat: [{ $toString: ["$nit"] }] }, //Para filtrar se convierte a string para que el regex funciones
+        tipoCorres: 1,
+        descripTipco: {
+          $concat: [{ $arrayElemAt: ["$tipco.descripcion", 0] }],
+        },
+        descrip: 1,
+        descripTer: { $concat: [{ $arrayElemAt: ["$ter.descrip", 0] }] },
+        ser: 1,
+        operdiri: 1,
+        dep: 1,
+        esta: 1,
+        descripEsta: {
+          $switch: {
+            branches: [
+              { case: { $eq: ["$esta", 1] }, then: "PENDIENTE LEER" },
+              { case: { $eq: ["$esta", 2] }, then: "LEÍDA SIN RESPUESTA" },
+              { case: { $eq: ["$esta", 3] }, then: "LEÍDA CON ÉXITO" },
+              { case: { $eq: ["$esta", 4] }, then: "RESPUESTA CONFIRMADA" },
+            ],
+            default: "SIN DESCRIPCION DE ESTADO",
+          },
+        },
+        codAuxco: 1,
+        codUnifun: 1,
+        proceden: 1,
+        oper: 1,
+        operModi: 1,
+        fechaModi: 1,
+        medio: 1,
+        numeroFact: 1,
+        nroGuia: 1,
+        perRec: 1,
+        monto: 1,
     })
     .match({codResp:codResp})
     get_response("rescorr",data[0],codResp,res);
