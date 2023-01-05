b7d279895a383439719e409a0df14336fca8f13e
Cristian David Hernandez Suarez <69637534+cristian-hernandez20@users.noreply.github.com>
2023-01-05 17:20:07 -0500

    config

--- src/models/CONFIG.ts
+++ src/models/CONFIG.ts
@@ -1,43 +1,44 @@
 import { Schema, model } from "mongoose";
-
 interface config {
   ips: Object;
   ubicacion: [];
-  fechaCrea: Date;
   fechaModif: Date;
+  fechaCrea: Date;
+  puerto: String;
 }
-
-const config_schema = new Schema<config>({
-  ips: {
-    ipPublica:{
-        type:String,
-        required:true
+const config_schema = new Schema<config>(
+  {
+    ips: {
+      ipPublica: {
+        type: String,
+        required: true,
+      },
+      ipLocal: {
+        type: String,
+        required: true,
+      },
     },
-    ipLocal:{
-        type:String,
-        required:true
-    }
+    ubicacion: [],
+    fechaCrea: {
+      type: Date,
+    },
+    fechaModif: {
+      type: Date,
+    },
+    puerto: {
+      type: String,
+    },
   },
-  ubicacion: [],
-  fechaCrea: {
-    type: Date,
-    required: true,
-  },
-  fechaModif: {
-    type: Date,
-    required: true,
-  },
-}, {versionKey:false});
+  { versionKey: false }
+);
 
 config_schema.pre("save", async function (next) {
+  const data = await config_model.findOne({});
 
-  const data = await config_model.findOne({})
+  if (data) next(new Error("Ya tiene configuracion"));
+  else next();
+});
 
-  if(data) next( new Error("Ya tiene configuracion"))
-  else next()
-  
-})
-
-config_schema.index({ips:1}, {unique:true})
+config_schema.index({ ips: 1 }, { unique: true });
 
 export const config_model = model<config>("config", config_schema);
